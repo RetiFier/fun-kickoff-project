070ffa633fbb99f2eaa15e3c6cece27ef7225f68
Reti Fier <50056219+RetiFier@users.noreply.github.com>
2022-01-06 23:48:14 -0800

    Revert "Dashboard skeleton"

--- client/src/components/AuthContainer/useStyles.ts
+++ client/src/components/AuthContainer/useStyles.ts
@@ -1,10 +1,12 @@
+import { Theme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   root: {
     minHeight: '100vh',
     height: '100%',
     justifyContent: 'center',
+    backgroundColor: theme.palette.secondary.main,
     '& .MuiInput-underline:before': {
       borderBottom: '1.2px solid rgba(0, 0, 0, 0.2)',
     },
